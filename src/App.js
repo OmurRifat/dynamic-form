659ded9df9761a625806a78909c029eeaaeaeca0
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-12-25 20:58:27 +0600

    data loaded & displayed at clint side

--- src/App.js
+++ src/App.js
@@ -149,7 +149,8 @@ function App() {
               aria-controls="menu"
               className="container-div outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
             >
-              <span className="pr-1 font-semibold flex-1">Dropdown</span>
+              <span
+                className="pr-1 font-semibold flex-1">{ selected?.MainSector }</span>
               <span>
                 <svg
                   className="fill-current h-4 w-4 transform group-hover:-rotate-180 transition duration-150 ease-in-out"
@@ -167,13 +168,97 @@ function App() {
               aria-hidden="true"
               className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32"
             >
-              <li className="rounded-sm px-3 py-1 hover:bg-gray-100">Programming</li>
+              {
+                sectors?.map((sector, id) => (
+                  <li
+                    key={ id }
+                    className="rounded-sm relative px-3 py-1 hover:bg-gray-100"
+                  >
+                    <div
+                      aria-haspopup="true"
+                      aria-controls="menu-lang"
+                      className="nested-menu w-full text-left flex items-center outline-none focus:outline-none"
+                    >
+                      <span className="pr-1 flex-1">{ sector?.MainSector }</span>
+                      {
+                        sector?.ParentSector && <span className="mr-auto">
+                          <svg
+                            className="fill-current h-4 w-4 transition duration-150 ease-in-out"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 20 20"
+                          >
+                            <path
+                              d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                            />
+                          </svg>
+                        </span>
+                      }
+                    </div>
+                    <ul
+                      id="menu-lang"
+                      aria-hidden="true"
+                      className="bg-white border rounded-sm absolute top-0 right-0 transition duration-150 ease-in-out origin-top-left min-w-32"
+                    >
+                      {/* { console.log(sector?.ParentSector, "from inside") } */ }
+                      {
+                        sector?.ParentSector?.map((subParentSector, id) => (
+                          <li key={ id } className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
+                            <div
+                              aria-haspopup="true"
+                              aria-controls="menu-lang-python"
+                              className=" nested-menu w-full text-left flex items-center outline-none focus:outline-none"
+                            >
+                              <span className="pr-1 flex-1">{ subParentSector?.SubParentSector }</span>
+                              {
+                                subParentSector?.SuperChildrenSector && <span className="mr-auto">
+                                  <svg
+                                    className="fill-current h-4 w-4 transition duration-150 ease-in-out"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                  >
+                                    <path
+                                      d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                                    />
+                                  </svg>
+                                </span>
+                              }
+                            </div>
+                            {
+
+                              console.log(subParentSector?.SuperChildrenSector, 'from children sector')
+                            }
+                            {
+                              subParentSector?.SuperChildrenSector && <ul
+                                id="menu-lang-python"
+                                aria-hidden="true"
+                                className="bg-white border rounded-sm absolute top-0 right-0 transition duration-150 ease-in-out origin-top-left min-w-32"
+                              >
+                                {
+                                  subParentSector?.SuperChildrenSector?.map((children, id) => (
+                                    <li className="px-3 py-1 hover:bg-gray-100">{ children.SubChildrenSector }</li>
+                                  ))
+                                }
+                              </ul>
+                            }
+                          </li>
+                        ))
+
+                      }
+                      <li className="px-3 py-1 hover:bg-gray-100">Javascript</li>
+
+                      <li className="px-3 py-1 hover:bg-gray-100">Go</li>
+                      <li className="px-3 py-1 hover:bg-gray-100">Rust</li>
+                    </ul>
+                  </li>
+                ))
+              }
+
               <li className="rounded-sm px-3 py-1 hover:bg-gray-100">DevOps</li>
-              <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
-                <button
+              {/* <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
+                <div
                   aria-haspopup="true"
                   aria-controls="menu-lang"
-                  className="w-full text-left flex items-center outline-none focus:outline-none"
+                  className="nested-menu w-full text-left flex items-center outline-none focus:outline-none"
                 >
                   <span className="pr-1 flex-1">Langauges</span>
                   <span className="mr-auto">
@@ -187,7 +272,7 @@ function App() {
                       />
                     </svg>
                   </span>
-                </button>
+                </div>
                 <ul
                   id="menu-lang"
                   aria-hidden="true"
@@ -195,10 +280,10 @@ function App() {
                 >
                   <li className="px-3 py-1 hover:bg-gray-100">Javascript</li>
                   <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
-                    <button
+                    <div
                       aria-haspopup="true"
                       aria-controls="menu-lang-python"
-                      className="w-full text-left flex items-center outline-none focus:outline-none"
+                      className=" nested-menu w-full text-left flex items-center outline-none focus:outline-none"
                     >
                       <span className="pr-1 flex-1">Python</span>
                       <span className="mr-auto">
@@ -212,7 +297,7 @@ function App() {
                           />
                         </svg>
                       </span>
-                    </button>
+                    </div>
                     <ul
                       id="menu-lang-python"
                       aria-hidden="true"
@@ -225,7 +310,7 @@ function App() {
                   <li className="px-3 py-1 hover:bg-gray-100">Go</li>
                   <li className="px-3 py-1 hover:bg-gray-100">Rust</li>
                 </ul>
-              </li>
+              </li> */}
               <li className="rounded-sm px-3 py-1 hover:bg-gray-100">Testing</li>
             </ul>
           </div>
@@ -236,9 +321,9 @@ function App() {
           </div>
           <button type='submit' className=' px-8 py-4 my-4 bg-purple-600'>Save</button>
 
-        </div>
-      </form>
-    </div>
+        </div >
+      </form >
+    </div >
   );
 }
 
