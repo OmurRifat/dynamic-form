5c3ba0a5b9597c98d5fed45d67b8227ee9ed06e2
OmurRifat <jabedomur.ece.ku@gmail.com>
2022-12-25 18:49:25 +0600

    basic structure for the secetors manue added

--- src/App.js
+++ src/App.js
@@ -37,23 +37,23 @@ function App() {
 
   // console.log(sectors);
   return (
-    <div className="App">
+    <div className=" w-5/6 md:w-3/4 mx-auto">
       <h4>Dynamic Form</h4>
-      <form onSubmit={ handleSubmit(onSubmit) } action="" className=' w-1/3 border mx-auto'>
+      <form onSubmit={ handleSubmit(onSubmit) } action="" className=' w-full md:w-1/2 border mx-auto relative'>
         <div >
           <h4>There will have some text.</h4>
           <label htmlFor="name">Your Name</label>
           <input required { ...register("name") } className=' border my-4 ml-2 focus:outline-none focus:border-purple-500' type="text" name="name" id="name" /> <br />
 
-          <label htmlFor="Sectors">Sectors</label>
+          <label htmlFor="Sectors">Sectors:</label>
           {/* <select required name="pets" id="Sectors" className=' border my-4 ml-2 focus:outline-none focus:border-purple-500'>
             <option value="">Please Select a Sectors</option>
             <option { ...register("sector") } value="Manufacturing">Manufacturing</option>
           </select> */}
 
-          <Listbox value={ selected } onChange={ setSelected }>
+          {/* <Listbox value={ selected } onChange={ setSelected }>
             <div className="relative mt-1">
-              <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md shadow-orange-600 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+              <Listbox.Button className="relative w-1/3 cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md shadow-orange-600 focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
                 <span className="block truncate">{ selected?.MainSector }</span>
                 <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                   <ChevronUpDownIcon
@@ -68,7 +68,7 @@ function App() {
                 leaveFrom="opacity-100"
                 leaveTo="opacity-0"
               >
-                <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <Listbox.Options className="absolute mt-1 max-h-60 w-1/3 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm min-w-32">
                   { sectors?.map((sector, personIdx) => (
                     <Listbox.Option
                       key={ personIdx }
@@ -99,12 +99,16 @@ function App() {
                       <ul
                         id='parent-sector'
                         aria-hidden='true'
-                        className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute 
-  transition duration-150 ease-in-out origin-top min-w-32"
+                        className=" group-hover:scale-100 absolute max-h-60 w-full overflow-auto rounded-md py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm transition duration-150 ease-in-out origin-top min-w-32"
                       >
-                        { sector?.ParentSector.map((subParentSector, id) => {
-
-                        }) }
+                        { sector?.ParentSector.map((subParentSector, id) => (
+                          <li
+                            key={ id }
+                            className="rounded-sm px-3 py-1 hover:bg-gray-100"
+                          >
+                            { subParentSector.SubParentSector }
+                          </li>
+                        )) }
                       </ul>
 
                     </Listbox.Option>
@@ -112,7 +116,119 @@ function App() {
                 </Listbox.Options>
               </Transition>
             </div>
-          </Listbox>
+          </Listbox> */}
+          {/* <button
+            aria-haspopup='true'
+            aria-controls='sector-parents'
+            className="outline-none  w-1/2 cursor-pointer focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
+          >
+            <span
+              className='pr-1 font-semibold flex-1'
+              value={ selected }
+              onChange={ setSelected }>
+              { selected?.MainSector }
+            </span>
+            <span className="pointer-events-none  flex items-center ml-2">
+              <ChevronUpDownIcon
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </span>
+          </button> */}
+          {/* <ul
+            id='sector-parents'
+            aria-hidden='true'
+            className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32"
+          >
+            <li className="rounded-sm px-3 py-1 hover:bg-gray-100">Programming</li>
+            <li className="rounded-sm px-3 py-1 hover:bg-gray-100">DevOps</li>
+          </ul> */}
+          <div className="group inline-block">
+            <div
+              aria-haspopup="true"
+              aria-controls="menu"
+              className="container-div outline-none focus:outline-none border px-3 py-1 bg-white rounded-sm flex items-center min-w-32"
+            >
+              <span className="pr-1 font-semibold flex-1">Dropdown</span>
+              <span>
+                <svg
+                  className="fill-current h-4 w-4 transform group-hover:-rotate-180 transition duration-150 ease-in-out"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                  />
+                </svg>
+              </span>
+            </div>
+            <ul
+              id="menu"
+              aria-hidden="true"
+              className="bg-white border rounded-sm transform scale-0 group-hover:scale-100 absolute transition duration-150 ease-in-out origin-top min-w-32"
+            >
+              <li className="rounded-sm px-3 py-1 hover:bg-gray-100">Programming</li>
+              <li className="rounded-sm px-3 py-1 hover:bg-gray-100">DevOps</li>
+              <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
+                <button
+                  aria-haspopup="true"
+                  aria-controls="menu-lang"
+                  className="w-full text-left flex items-center outline-none focus:outline-none"
+                >
+                  <span className="pr-1 flex-1">Langauges</span>
+                  <span className="mr-auto">
+                    <svg
+                      className="fill-current h-4 w-4 transition duration-150 ease-in-out"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                      />
+                    </svg>
+                  </span>
+                </button>
+                <ul
+                  id="menu-lang"
+                  aria-hidden="true"
+                  className="bg-white border rounded-sm absolute top-0 right-0 transition duration-150 ease-in-out origin-top-left min-w-32"
+                >
+                  <li className="px-3 py-1 hover:bg-gray-100">Javascript</li>
+                  <li className="rounded-sm relative px-3 py-1 hover:bg-gray-100">
+                    <button
+                      aria-haspopup="true"
+                      aria-controls="menu-lang-python"
+                      className="w-full text-left flex items-center outline-none focus:outline-none"
+                    >
+                      <span className="pr-1 flex-1">Python</span>
+                      <span className="mr-auto">
+                        <svg
+                          className="fill-current h-4 w-4 transition duration-150 ease-in-out"
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 20 20"
+                        >
+                          <path
+                            d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+                          />
+                        </svg>
+                      </span>
+                    </button>
+                    <ul
+                      id="menu-lang-python"
+                      aria-hidden="true"
+                      className="bg-white border rounded-sm absolute top-0 right-0 transition duration-150 ease-in-out origin-top-left min-w-32"
+                    >
+                      <li className="px-3 py-1 hover:bg-gray-100">2.7</li>
+                      <li className="px-3 py-1 hover:bg-gray-100">3+</li>
+                    </ul>
+                  </li>
+                  <li className="px-3 py-1 hover:bg-gray-100">Go</li>
+                  <li className="px-3 py-1 hover:bg-gray-100">Rust</li>
+                </ul>
+              </li>
+              <li className="rounded-sm px-3 py-1 hover:bg-gray-100">Testing</li>
+            </ul>
+          </div>
 
           <div>
             <input { ...register("termsCondition") } type="checkbox" className=' border border-purple-500' />
